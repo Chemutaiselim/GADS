bb6e70f108f9cdde99a691d1f9ebe67557eabb38
Selim <selimchemutai19@gmail.com>
2021-08-03 22:34:03 +0300

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,5 @@
-fun main (help: Array<String>){
+fun main (args: Array<String>){
+    // it has a function, main and the variable args which isi an array of string.
 
     println("Hello, World")
 }
