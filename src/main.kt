8acc21a6192f0a39258fde1b3f6f6b040fff4381
Selim <selimchemutai19@gmail.com>
2021-08-03 23:05:23 +0300

    kotlin

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,26 @@
 fun main (args: Array<String>){
     // it has a function, main and the variable args which isi an array of string.
 
+    var q= Question()
+    //to use the question class
     println("Hello, World")
+
+    q.Awnser="42"
+
+    println("You said the awnser is ${q.Awnser}")
+   //This string template combines a compound variable.
+}
+
+class Question{
+    //In kotlin, you can create a class inside the file you had the function
+    // theres no need to create a new file
+     var Awnser="string"
+
+    fun display(){
+        println("You said"+ Awnser)
+        // in kotlin you can use the method below to print a variable
+
+        println("you said $Awnser")
+    }
+
 }
