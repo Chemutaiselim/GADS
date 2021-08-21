97193575602135b809e831d233e13f8d67f7190b
Selim <selimchemutai19@gmail.com>
2021-08-21 21:49:48 +0300

    Extension Functions

--- src/Named_parameters.kt
+++ src/Named_parameters.kt
@@ -1,2 +1,13 @@
 //Kotlin Supports naming of our parameters
-fun main()
+fun main(){
+    named_parameters(message = "Hello,World",secondPar = 88)
+    named_parameters(secondPar = 77,message ="Hello, World")
+
+    //we cant mix named parameters and unnamed parameters
+    //Benefits, we can change the positions of the parameters and
+    // also keep track of which parameters we are using
+    //we can also use default parameters here.
+}
+fun named_parameters(message:String,secondPar:Int=99){
+    println(message + secondPar)
+}
