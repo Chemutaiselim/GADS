ebb322de50c4f73367ca6986a9e99ab2d9ad3d25
Selim <selimchemutai19@gmail.com>
2021-08-22 16:29:10 +0300

    Infix functions

--- src/Infix_Functions.kt
+++ src/Infix_Functions.kt
@@ -8,7 +8,16 @@ fun main(args:Array<String>){
     val h2=Header("H2")
 
     val h3=h1.plus(h2)
+
+    val h4=h1 plus2 h2
+    /*
+  we dont need to use dot and brackets after using the infix lable
+   */
+
+    val h5=h1 + h2
+
     println(h3.Name)
+    println(h4.Name)
 
 }
 
@@ -21,5 +30,13 @@ fun Header.plus(other:Header):Header{
     this is an extension function used to add two functions together
     and return a new header
      */
+    return Header(this.Name+other.Name)
+}
+
+infix fun Header.plus2(other:Header):Header{
+    return Header(this.Name+other.Name)
+}
+
+operator infix fun Header.plus3(other:Header):Header{
     return Header(this.Name+other.Name)
 }
