8d420a995fc2fb571685a61908a26fc5fa7e95e7
Selim <selimchemutai19@gmail.com>
2021-08-23 21:58:56 +0300

    Kotlin functions summary

---
+++ src/Summary.kt
@@ -0,0 +1,9 @@
+/*
+Kotlin has much easier to use functions,
+we dont need classes
+we can use default and named parameters
+on of the most powerful features is that we can create extension functions
+We can aslo create infix functions
+Supports for overloaded operators
+kotlin supports tail recursion
+ */
--- src/Tail_Recursive_functions.kt
+++ src/Tail_Recursive_functions.kt
@@ -8,14 +8,15 @@ they have to be the correct form
  */
 
 fun main(args:Array<String>){
-    println(fib(10, BigInteger("1"), BigInteger("0")))
+    println(fib(50000, BigInteger("1"), BigInteger("0")))
 }
 
-fun fib(n:Int,a:BigInteger,b:BigInteger):BigInteger{
+tailrec fun fib(n:Int,a:BigInteger,b:BigInteger):BigInteger{
     return if (n==0)b else fib(n -1,a+b, a)
 }
 /*
 function will add the last two integers to get the next value i.e.
 1,1,2,3,5,8.13
 the function is recursive
+We add the tail rec key words to make it recursive
  */
