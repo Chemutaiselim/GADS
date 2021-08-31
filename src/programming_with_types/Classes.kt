0e6a29faef787422db8d65c566a21995b512f93f
Selim <selimchemutai19@gmail.com>
2021-08-31 11:41:21 +0300

    Update Classes.kt

--- src/programming_with_types/Classes.kt
+++ src/programming_with_types/Classes.kt
@@ -1,4 +1,17 @@
 package programming_with_types
 
 class Classes {
+    /*
+    Kotlin is object oriented
+    Hence it supports classes
+    classes in kotlin are public by default, they are also final, stop somebody from
+    inheriting them,by default
+    Kotlin accepts abstract classes, uses modifiers, supports sealed classes, constructors
+    and data classes
+
+     */
+    /*
+
+     */
 }
+
