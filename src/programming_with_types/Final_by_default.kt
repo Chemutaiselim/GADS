74837b7e00296fcd87177fce9be0cfce456defa2
Selim <selimchemutai19@gmail.com>
2021-08-31 11:56:07 +0300

    Update Final_by_default.kt

--- src/programming_with_types/Final_by_default.kt
+++ src/programming_with_types/Final_by_default.kt
@@ -1,5 +1,15 @@
 package programming_with_types
 
-class Final_by_default {
+open class Final_by_default {
+    var firstName:String=""
+    var lastName:String=""
+
+    open fun getName() :String ="$firstName $lastName"
+}
+class student:Final_by_default(){
+    //The compiler complains because the final by default class is final meaning,it cant be inherted from
+    //we use the 'open' key word to allow student to derive from person
+    //we also need to use the open key word on the function in order to override it.
+    override fun getName():String{return ""}
 
 }
