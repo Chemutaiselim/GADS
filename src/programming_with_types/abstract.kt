61e883082041b3439b21cd3913f29e9879e0b272
Selim <selimchemutai19@gmail.com>
2021-08-31 12:13:07 +0300

    Update abstract.kt

--- src/programming_with_types/abstract.kt
+++ src/programming_with_types/abstract.kt
@@ -1,4 +1,17 @@
 package programming_with_types
 
-class abstract {
+abstract class abstract {
+    var firstName:String=""
+    var secondName:String=""
+    abstract fun getAdress():String
+    open fun getName():String="$firstName $secondName"
+}
+class secondClass:abstract(){
+    override fun getAdress(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ""
+    }
+
+    override fun getName():String {
+        return ""}
 }
