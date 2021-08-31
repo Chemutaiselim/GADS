47294db0370524861ce33b67cec948fe157a302d
Selim <selimchemutai19@gmail.com>
2021-08-31 12:33:00 +0300

    Update sealed_CLasses.kt

--- src/programming_with_types/sealed_CLasses.kt
+++ src/programming_with_types/sealed_CLasses.kt
@@ -1,4 +1,21 @@
 package programming_with_types
 
-class sealed_CLasses {
+sealed class sealed_CLasses {
+    /*
+    Algebraic data class
+    You can specify exactly the set of derived classes from an abstract class
+    'ENums on steroids'
+    Restricted type of derived classes
+    you can use sealed classes instead of enum
+     */
+    class awake:sealed_CLasses()
+    class asleep:sealed_CLasses()
+    class eating(val food:String):sealed_CLasses()
+}
+fun handlePersonEvent(event:sealed_CLasses){
+    when(event){
+        is sealed_CLasses.awake-> println("Awake")
+        is sealed_CLasses.asleep-> println("Asleep")
+        is sealed_CLasses.eating-> println(event.food)
+    }
 }
